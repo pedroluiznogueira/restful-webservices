17688f01b2a9a6e1df28153ee82a91f0c04a2b4e
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-07 22:00:06 -0300

    Enhacing a path variable in a request

--- src/main/java/com/rest/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/rest/restfulwebservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.rest.restfulwebservices.controller;
 
 import com.rest.restfulwebservices.model.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping("bean")
     public HelloWorld getHelloBean() {
         return new HelloWorld("Hello bean");
+    }
+
+    @GetMapping("param/{name}")
+    public String getHelloParam(@PathVariable ("name") String name) {
+        return name;
     }
 }
