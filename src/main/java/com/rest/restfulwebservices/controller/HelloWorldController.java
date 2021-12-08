0fffd9291dfe23839892ac84a316d35e490598e2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-07 21:41:48 -0300

    Returning a bean in a request

--- src/main/java/com/rest/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/rest/restfulwebservices/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.rest.restfulwebservices.controller;
 
+import com.rest.restfulwebservices.model.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class HelloWorldController {
     @GetMapping
     public String getHello() {
         return "Hello world";
+    }
+
+    @GetMapping("bean")
+    public HelloWorld getHelloBean() {
+        return new HelloWorld("Hello bean");
     }
 }
