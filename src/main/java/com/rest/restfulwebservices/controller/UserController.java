da99ca04cc512c0e3d56f4ccd2b4563ae59232e2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-13 20:19:52 -0300

    The same thing with the annotation, doesn't matter if the controller catches the Exception but I don't use the Exception

--- src/main/java/com/rest/restfulwebservices/controller/UserController.java
+++ src/main/java/com/rest/restfulwebservices/controller/UserController.java
@@ -25,6 +25,9 @@ public class UserController {
     @GetMapping("getById/{id}")
     public ResponseEntity<Optional<User>> getById(@PathVariable ("id") Long id) {
         Optional<User> user = userService.getById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id-" + id);
+        }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
--- src/main/java/com/rest/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/rest/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.util.Date;
 
-@ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
